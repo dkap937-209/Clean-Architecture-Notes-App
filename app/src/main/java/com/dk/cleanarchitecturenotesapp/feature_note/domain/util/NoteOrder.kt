daf674b8a61cecf2c43064132df0983088741231
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-02-26 16:17:50 +1300

    Created components for notes page

--- app/src/main/java/com/dk/cleanarchitecturenotesapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/dk/cleanarchitecturenotesapp/feature_note/domain/util/NoteOrder.kt
@@ -1,8 +1,18 @@
 package com.dk.cleanarchitecturenotesapp.feature_note.domain.util
 
+import com.dk.cleanarchitecturenotesapp.feature_note.presentation.notes.NotesEvent
+
 sealed class NoteOrder(val orderType: OrderType) {
     class Title(orderType: OrderType): NoteOrder(orderType)
     class Date(orderType: OrderType): NoteOrder(orderType)
     class Colour(orderType: OrderType): NoteOrder(orderType)
+
+    fun copy(orderType: OrderType): NoteOrder{
+        return when(this){
+            is Colour -> Colour(orderType = orderType)
+            is Date -> Date(orderType = orderType)
+            is Title -> Title(orderType = orderType)
+        }
+    }
 
 }
---
+++ app/src/main/java/com/dk/cleanarchitecturenotesapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,61 @@
+package com.dk.cleanarchitecturenotesapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dk.cleanarchitecturenotesapp.feature_note.domain.util.NoteOrder
+import com.dk.cleanarchitecturenotesapp.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+
+    Column(
+        modifier = modifier
+    ){
+        Row(
+            modifier = modifier
+        ){
+            DefaultRadioButton(
+                text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = { onOrderChange(NoteOrder.Title(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(noteOrder.orderType)) }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                text = "Colour",
+                selected = noteOrder is NoteOrder.Colour,
+                onSelect = { onOrderChange(NoteOrder.Colour(noteOrder.orderType)) }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                DefaultRadioButton(
+                    text = "Ascending",
+                    selected = noteOrder.orderType is OrderType.Ascending,
+                    onSelect = { onOrderChange(noteOrder.copy(OrderType.Ascending)) }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                DefaultRadioButton(
+                    text = "Descending",
+                    selected = noteOrder.orderType is OrderType.Descending,
+                    onSelect = { onOrderChange(noteOrder.copy(OrderType.Descending)) }
+                )
+            }
+
+        }
+    }
+
+
+}
