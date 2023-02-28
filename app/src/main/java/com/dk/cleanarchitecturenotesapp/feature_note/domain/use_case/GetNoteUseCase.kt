b6530e3aa35962862c47a86c13d34b2f91e1f341
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-02-28 18:24:51 +1300

    Created use case for getting a single note

---
+++ app/src/main/java/com/dk/cleanarchitecturenotesapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -0,0 +1,13 @@
+package com.dk.cleanarchitecturenotesapp.feature_note.domain.use_case
+
+import com.dk.cleanarchitecturenotesapp.feature_note.domain.model.Note
+import com.dk.cleanarchitecturenotesapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note?{
+        return repository.getNotById(id = id)
+    }
+}
